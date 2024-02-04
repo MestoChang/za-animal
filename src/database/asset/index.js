cea7e2047638f8629027baa3ac3dddbd0a3b23b0
Julia <s11003028@gmail.com>
2024-02-04 13:31:32 +0800

    upload img

--- src/database/asset/index.js
+++ src/database/asset/index.js
@@ -4,6 +4,22 @@ import Result03 from './Result_03.jpg';
 import Result04 from './Result_04.jpg';
 import Result05 from './Result_05.jpg';
 import Result06 from './Result_06.jpg';
+import Q1 from './Q1.png';
+import Q2 from './Q2.png';
+import Q3 from './Q3.png';
+import Q4 from './Q4.png';
+import Q5 from './Q5.png';
+import Q6 from './Q6.png';
+import Q7 from './Q7.png';
+import Q8 from './Q8.png';
+import Q9 from './Q9.png';
+import Q10 from './Q10.png';
+import Q11 from './Q11.png';
+import QuizBottom from './quizbottom.png';
+import ResultBottom from './resultbottom.png';
+import Invatation from './invatation.png';
+import Logo from './logo.png';
+
 
 export {
     Result01,
@@ -11,5 +27,20 @@ export {
     Result03,
     Result04,
     Result05,
-    Result06
+    Result06,
+    Q1,
+    Q2,
+    Q3,
+    Q4,
+    Q5,
+    Q6,
+    Q7,
+    Q8,
+    Q9,
+    Q10,
+    Q11,
+    QuizBottom,
+    ResultBottom,
+    Invatation,
+    Logo
 }
